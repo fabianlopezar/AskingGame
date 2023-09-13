5a4e2bf9e6a4c18b93c3c3cd3b650eadd654133e
Fabian Lopez <65512494+fabianlopezar@users.noreply.github.com>
2023-09-12 22:09:46 -0500

    [ADD] Controlador de preguntas.

--- Assets/Scripts/ReadTxtFile.cs
+++ Assets/Scripts/ReadTxtFile.cs
@@ -2,13 +2,35 @@ using UnityEngine;
 using System.IO;
 using System.Collections.Generic;
 using Models;
+using TMPro;
+using UnityEngine.UI;
+
 public class ReadTxtFile : MonoBehaviour
 {
     public TextAsset _fileQuestions;
     private List<PreguntasMultiples> _listQuestions = new List<PreguntasMultiples>();
+    [Header("Textos")]
+    public TMP_Text _opc1Text;
+    public TMP_Text _opc2Text;
+    public TMP_Text _opc3Text;
+    public TMP_Text _opc4Text;
+    public TMP_Text _questionText;
+    [Header("Paneles")]
+    public  GameObject _panelGameOver;
+    public  GameObject _panelGameWin;
+    public int _indice=0;
+
     private void Start()
     {
-     
+        _panelGameOver.SetActive(false);
+        _panelGameWin.SetActive(false);    
+        FileReading();
+        UpdateUIQuestions();
+   
+    }
+
+    public void FileReading()
+    {
         if (_fileQuestions != null)
         {
             //Split the file into lines.
@@ -18,7 +40,7 @@ public class ReadTxtFile : MonoBehaviour
                 string[] parts = elem.Split("-");
                 if (parts.Length == 7)// Make sure there are 8 parts (question, options, correct answer, verse, state)
                 {
-                  
+
                     string question = parts[0].Trim();
                     string opc1 = parts[1].Trim();
                     string opc2 = parts[2].Trim();
@@ -33,13 +55,59 @@ public class ReadTxtFile : MonoBehaviour
                     _listQuestions.Add(preguntaMultiple);
                 }
             }
-            foreach (PreguntasMultiples elem in _listQuestions) 
-            {
-                Debug.Log("Pregunta: "+ elem.Pregunta);
-                Debug.Log("Opcion Correcta: "+elem.OpcCorrecta);
-            }
-            
         }
-        Debug.Log("SOY LA LISTA DE PREGUNTAS" + _listQuestions);
+    }
+
+    public void UpdateUIQuestions()
+    {
+        _opc1Text.text = _listQuestions[_indice].Opc1;
+        _opc2Text.text = _listQuestions[_indice].Opc2;
+        _opc3Text.text = _listQuestions[_indice].Opc3;
+        _opc4Text.text = _listQuestions[_indice].Opc4;
+        _questionText.text = _listQuestions[_indice].Pregunta;
+       
+    }
+    public void OnButtonClick(TMP_Text pregunta)
+    {
+        if (pregunta.text == _listQuestions[_indice].OpcCorrecta)
+        {
+            CorrectResponse();
+        }
+        else
+        {
+            IncorrectResponse();
+        }
+    }
+    public void CorrectResponse()
+    {
+        _panelGameWin.SetActive(true);
+    }
+    public void IncorrectResponse()
+    {
+        _panelGameOver.SetActive(true);
+    }
+   
+    public void HidePanelGameOver()
+    {
+        _panelGameOver.SetActive(false);
+    }
+    public void NextQuestion()
+    {
+        _panelGameWin.SetActive(false);
+        _indice++;
+        if (_listQuestions[_indice] == null)
+        {
+            _indice = 0;
+            NextQuestion();
+        }
+        else
+        {
+            UpdateUIQuestions();
+            Debug.Log("SOY el indice:" + _indice);
+            Debug.Log("SOY LA RESPUESTA: " + _listQuestions[_indice].OpcCorrecta);
+        }
+       
+       
+       
     }
 }
